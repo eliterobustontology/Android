1abf9ed9bf7c71346d95a7598def838aa43509b2
eroinnovations <eroinnovations9@gmail.com>
2025-06-14 08:27:34 +0300

    Updates

--- app/src/main/assets/drivers.js
+++ app/src/main/assets/drivers.js
@@ -156,3 +156,18 @@ function changeWebViewBackground(Colors) {
     Android.setSystemBarColor(Colors, Colors);
 
 };
+
+const BATTERY = (callback) => {
+    var batteryStatus = Android.getBatteryStatus();
+    callback(batteryStatus);
+};
+
+const VIBRATION = (TIME) => {
+    if (localStorage.getItem("Environment") === "Production") {
+        Android.vibrate(TIME || 500);
+    }
+};
+
+const NOTIFICATION = (TITLE, MESSAGE) => {
+    Android.showNotification(TITLE, MESSAGE);
+};
